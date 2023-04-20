f7caf2c18caf529c35dcc75eb35a81e94b9fc8b5
Sean L <leonardmsean@gmail.com>
2023-04-20 15:11:26 -0400

    added questions and tests section with accompanying sections in write file

--- Develop/index.js
+++ Develop/index.js
@@ -1,121 +1,148 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+let licenseBadgeUrl = "";
+let licenseDescription = "";
 
 inquirer
   .prompt([
     {
-      type: 'input',
-      name: 'title',
-      message: 'What would you like the title of your README to be?',
+      type: "input",
+      name: "title",
+      message: "What would you like the title of your README to be?",
     },
     {
-      type: 'input',
-      name: 'description',
-      message: 'Why did you create this project? Include what problems it solves and what you learned building it.',
+      type: "input",
+      name: "description",
+      message:
+        "Why did you create this project?  Include what problems it solves and what you learned building it.",
     },
     {
-      type: 'confirm',
-      name: 'toc',
-      message: 'Would you like to include a table of contents?',
+      type: "input",
+      name: "installation",
+      message: "How do you install this project?",
     },
     {
-      type: 'input',
-      name: 'installation',
-      message: 'How do you install this project?',
+      type: "input",
+      name: "usage",
+      message: "What is the intended use of the project?",
     },
     {
-      type: 'input',
-      name: 'usage',
-      message: 'What is the intended use of the project?',
+      type: "list",
+      name: "license",
+      message: "What, if any, license have you attached to the project?",
+      choices: [
+        "Apache License 2.0",
+        "GNU General Public License 2.0",
+        "MIT License",
+        "Boost Software License 1.0",
+        "Eclipse Public License 2.0",
+        "None",
+      ],
     },
     {
-      type: 'input',
-      name: 'credit',
-      message: 'What collaborators or 3rd party applications would you like to acknowledge?',
+      type: "input",
+      name: "credit",
+      message:
+        "What collaborators or 3rd party applications would you like to acknowledge?",
+    },
+
+    {
+      type: "input",
+      name: "tests",
+      message: "Please provide instructions for any tests",
     },
     {
-      type: 'list',
-      name: 'license',
-      message: 'What, if any, license have you attached to the project?',
-      choices: ['Apache License 2.0', 'GNU General Public License 2.0', 'MIT License','Boost Software License 1.0', 'Eclipse Public License 2.0', 'None'],
+      type: "input",
+      name: "contribute",
+      message:
+        "Please tell other developers how they can contribute to the project.",
     },
     {
-      type: 'input',
-      name: 'features',
-      message: 'Please tell us about any additional features of the project you want to highlight.',
+      type: "input",
+      name: "url",
+      message: "What is the URL of your project?",
     },
     {
-      type: 'input',
-      name: 'contribute',
-      message: 'Please tell other developers how they can contribute to the project.',
+      type: "input",
+      name: "github",
+      message: "What is your github user name?",
     },
     {
-      type: 'input',
-      name: 'url',
-      message: 'What is the URL of your project?',
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
     },
   ])
-
   .then((data) => {
-    let licenseBadgeUrl = '';
-    let licenseDescription = '';
+    let licenseBadgeUrl = "";
+    let licenseDescription = "";
     switch (data.license) {
-      case 'Apache License 2.0':
-        licenseBadgeUrl = 'https://img.shields.io/badge/License-Apache%202.0-blue.svg';
-        licenseDescription = 'This project is licensed under the Apache License 2.0.';
+      case "Apache License 2.0":
+        licenseBadgeUrl =
+          "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
+        licenseDescription =
+          "This project is licensed under the Apache License 2.0.";
         break;
-      case 'GNU General Public License 2.0':
-        licenseBadgeUrl = 'https://img.shields.io/badge/License-GPLv2-blue.svg';
-        licenseDescription = 'This project is licensed under the GNU General Public License 2.0.';
+      case "GNU General Public License 2.0":
+        licenseBadgeUrl = "https://img.shields.io/badge/License-GPLv2-blue.svg";
+        licenseDescription =
+          "This project is licensed under the GNU General Public License 2.0.";
         break;
-      case 'MIT License':
-        licenseBadgeUrl = 'https://img.shields.io/badge/License-MIT-yellow.svg';
-        licenseDescription = 'This project is licensed under the MIT License.';
+      case "MIT License":
+        licenseBadgeUrl = "https://img.shields.io/badge/License-MIT-yellow.svg";
+        licenseDescription = "This project is licensed under the MIT License.";
         break;
-      case 'Boost Software License 1.0':
-        licenseBadgeUrl = 'https://img.shields.io/badge/License-Boost%201.0-lightblue.svg';
-        licenseDescription = 'This project is licensed under the Boost Software License 1.0.';
+      case "Boost Software License 1.0":
+        licenseBadgeUrl =
+          "https://img.shields.io/badge/License-Boost%201.0-lightblue.svg";
+        licenseDescription =
+          "This project is licensed under the Boost Software License 1.0.";
         break;
-      case 'Eclipse Public License 2.0':
-        licenseBadgeUrl = 'https://img.shields.io/badge/License-EPL%202.0-red.svg';
-        licenseDescription = 'This project is licensed under the Eclipse Public License 2.0.';
+      case "Eclipse Public License 2.0":
+        licenseBadgeUrl =
+          "https://img.shields.io/badge/License-EPL%202.0-red.svg";
+        licenseDescription =
+          "This project is licensed under the Eclipse Public License 2.0.";
         break;
-      case 'None':
-        licenseBadgeUrl = '';
-        licenseDescription = '';
+      case "None":
+        licenseBadgeUrl = "";
+        licenseDescription = "";
         break;
     }
 
-    let toc = '';
+    let toc = "";
     if (data.toc) {
-       toc = 
-        `## Table of Contents
+      toc = `## Table of Contents
 
         - [Description]
         - [Installation]
         - [Usage]
         - [Credits]
         - [License]
+        - [Tests]
         - [Features]
         - [Contributing]
-        - [App url]`;
+        - [App url]
+        - [Questions?]`;
     }
 
     // Construct the license section of the markdown with the badge URL
-    const licenseMarkdown = licenseBadgeUrl ? `[![License](${licenseBadgeUrl})](${data.url})\n\n${licenseDescription}` : '';
+    const licenseMarkdown = licenseBadgeUrl
+      ? `[![License](${licenseBadgeUrl})](${data.url})\n\n${licenseDescription}`
+      : "";
 
-    const filename = 'README.md';
+    const filename = "README.md";
     const markdown = `
     
     # ${data.title}
-    
-    ${toc}
-    
+
     ## Description
     
     ${data.description}
-
-       
+    
+    ${toc}
+    
     ## Installation
 
     ${data.installation}
@@ -123,18 +150,24 @@ inquirer
     ## Usage
     
     ${data.usage}
+
+    ## License
+    
+    ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+<br />
+This application is covered by the ${data.license} license. 
     
     ## Credits
     
-    ${data.credit}
-    
-    ## License
-    
-    ${licenseMarkdown}
-    
-    ## Addtional Features
+    ${data.credit}  
+  
+   ## Addtional Features
     
     ${data.features}
+
+    ## Tests
+
+    ${data.tests}
     
     ## How to Contribute
     
@@ -142,13 +175,19 @@ inquirer
     
     ## App url
     
-    ${data.url}`;
+    ${data.url}
+    
+    ## Questions?
+    
+    Come see me on GitHub @ [${data.github}](https://github.com/${data.github});
+    Or
+    Email me @ ${data.email}`;
 
     fs.writeFile(filename, markdown, (err) => {
       if (err) {
         console.log(error);
       } else {
-        console.log(`Created ${filename}`)
+        console.log(`Created ${filename}`);
       }
     });
   });
