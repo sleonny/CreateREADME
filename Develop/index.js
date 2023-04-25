0db32b481ce00da70dca38f52e19f72ebbf059f6
Sean L <leonardmsean@gmail.com>
2023-04-25 17:31:18 -0400

    everything is good to go I think

--- Develop/index.js
+++ Develop/index.js
@@ -4,73 +4,74 @@ const fs = require("fs");
 let licenseBadgeUrl = "";
 let licenseDescription = "";
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "title",
-    message: "What would you like the title of your README to be?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message:
-      "Why did you create this project?  Include what problems it solves and what you learned building it.",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "How do you install this project?",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "What is the intended use of the project?",
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "What, if any, license have you attached to the project?",
-    choices: [
-      "Apache License 2.0",
-      "GNU General Public License 2.0",
-      "MIT License",
-      "Boost Software License 1.0",
-      "Eclipse Public License 2.0",
-      "None",
-    ],
-  },
-  {
-    type: "input",
-    name: "credit",
-    message:
-      "What collaborators or 3rd party applications would you like to acknowledge?",
-  },
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What would you like the title of your README to be?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message:
+        "Why did you create this project?  Include what problems it solves and what you learned building it.",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "How do you install this project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is the intended use of the project?",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What, if any, license have you attached to the project?",
+      choices: [
+        "Apache License 2.0",
+        "GNU General Public License 2.0",
+        "MIT License",
+        "Boost Software License 1.0",
+        "Eclipse Public License 2.0",
+        "None",
+      ],
+    },
+    {
+      type: "input",
+      name: "credit",
+      message:
+        "What collaborators or 3rd party applications would you like to acknowledge?",
+    },
 
-  {
-    type: "input",
-    name: "tests",
-    message: "Please provide instructions for any tests",
-  },
-  {
-    type: "input",
-    name: "contribute",
-    message:
-      "Please tell other developers how they can contribute to the project.",
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "What is your github user name?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is your email address?",
-  },
-]);
-
-const filename = "README.md";
-const markdown = `
+    {
+      type: "input",
+      name: "tests",
+      message: "Please provide instructions for any tests",
+    },
+    {
+      type: "input",
+      name: "contribute",
+      message:
+        "Please tell other developers how they can contribute to the project.",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your github user name?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+    },
+  ])
+  .then((data) => {
+    const filename = "README.md";
+    const markdown = `
     
     # ${data.title}
 
@@ -122,10 +123,14 @@ const markdown = `
     Or
     Email me @ ${data.email}`;
 
-fs.writeFile(filename, markdown, (err) => {
-  if (err) {
+    fs.writeFile(filename, markdown, (err) => {
+      if (err) {
+        console.log(error);
+      } else {
+        console.log(`Created ${filename}`);
+      }
+    });
+  })
+  .catch((error) => {
     console.log(error);
-  } else {
-    console.log(`Created ${filename}`);
-  }
-});
+  });
